247840274a74bc2bdc03f9728ee12a97c38479c4
Aiden Pearce <104835550+Artemon4ik8091@users.noreply.github.com>
2024-09-26 16:03:07 +0300

    Add files via upload

---
+++ OSX-KVM-automated.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  OSX-KVM-automated.sh
+#  
+#
+#  Created by Aiden Pearce on 26.09.2024.
+#
+
+sudo apt install qemu-system uml-utilities virt-manager git dmg2img -y; cd; git clone --depth 1 --recursive https://github.com/kholia/OSX-KVM.git; cd OSX-KVM; ./fetch-macOS-v2.py; dmg2img -i BaseSystem.dmg BaseSystem.img; qemu-img create -f qcow2 mac_hdd_ng.img 256G; ./OpenCore-Boot.sh;
+
